0cdf6172f919b62179a44474da3f9242534f5bbb
Felix Halstrup <fh077327@fh-muenster.de>
2022-04-05 21:05:14 +0200

    Sockel eröffnen, auf Connections warten

--- main.c
+++ main.c
@@ -1,6 +1,55 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 4567
 
 int main() {
-    printf("Hello, World!\n");
+    int server_fd, new_socket, value;
+    int opt = 1;
+
+    char *print = "Server erfolgreich verbunden!";
+    struct sockaddr_in address;
+
+    int addrlen = sizeof(address);
+
+    //Socket filedescriptor erstellen
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        perror("Socket creation failed...");
+        exit(1);
+    }
+    //Socket an Port binden
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address))<0)
+    {
+        perror("Bind fehlgeschlagen");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("Listen fehlgeschlagen");
+        exit(EXIT_FAILURE);
+    }
+    //Neue Connections annehmen
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t*)&addrlen))<0)
+    {
+        perror("Connection fehlgeschlagen...");
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
+
+
